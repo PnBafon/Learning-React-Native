9a164310c760795a791b08dbf3c57b5629bf1e56
PnBafon <bafonprecious@gmail.com>
2025-03-15 00:40:15 +0100

    adding goals and displaying them

--- CourseObjectives.js
+++ CourseObjectives.js
@@ -1,68 +1,73 @@
-import { useState } from 'react';
-import {View, Text, TextInput, Button} from 'react-native'
-export default function CourseObjectives(){
-  const [inputGoal, setInputGoal] = useState('')
-  const [goals, setGoals] = useState([])
+import { useState } from "react";
+import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+export default function CourseObjectives() {
+  const [inputGoal, setInputGoal] = useState("");
+  const [goals, setGoals] = useState([]);
 
-  const handleInputGoal = (enteredText) =>{
-    setInputGoal(enteredText)
-  }
+  const handleInputGoal = (enteredText) => {
+    setInputGoal(enteredText);
+  };
 
-  const handleUpdateGoals = ()=>{
+  const handleUpdateGoals = () => {
     setGoals((currentGoals) => [...currentGoals, inputGoal]);
-    setInputGoal('')
-  }
+    setInputGoal("");
+  };
 
-  const dropGoal = (index) =>{
-    setGoals((goals) => goals.filter((goal,index) => console.log(goal,index)))
-  }
-    return (
-      <View style={{ paddingTop: 45, paddingHorizontal: 18 }}>
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
-          <TextInput
-            placeholder="Course goals"
-            style={{
-              borderBottomColor: "black",
-              borderBottomWidth: 1,
-              marginBottom: 3,
-              padding: 8,
-              width: "80%",
-            }}
-            value={inputGoal}
-            onChangeText={handleInputGoal}
-          />
-          <Button title="ADD" onPress={handleUpdateGoals} />
-        </View>
-        <View>
-          {goals.map((goal, index) => {return (
-            <Todo goal={goal} index={index}  key={index} />
-          );}
-            
-          )}
-        </View>
-      </View>
+  const dropGoal = (index) => {
+    setGoals((goals) =>
+      goals.filter((goal, index) => console.log(goal, index))
     );
+  };
+  return (
+    <View style={{ paddingTop: 45, paddingHorizontal: 18 }}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <TextInput
+          placeholder="Course goals"
+          style={{
+            borderBottomColor: "black",
+            borderBottomWidth: 1,
+            marginBottom: 3,
+            padding: 8,
+            width: "80%",
+          }}
+          value={inputGoal}
+          onChangeText={handleInputGoal}
+        />
+        <Button title="ADD" onPress={handleUpdateGoals} />
+      </View>
+      <View>
+        {goals.map((goal, index) => {
+          return <Todo goal={goal} index={index} key={index} />;
+        })}
+      </View>
+    </View>
+  );
 }
 
 const Todo = ({ goal, index, drop }) => {
   return (
-    <View
-      style={{
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-        marginTop: 8,
-      }}
-    >
-      <Text>{goal}</Text>
-
+    <View style={styles.listItem}>
+      <Text style={{ color: "black" }}>{goal}</Text>
       <Button title="remove" />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  listItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginVertical: 4,
+    borderWidth: 1,
+    borderColor: "gray",
+    borderRadius: 3,
+    paddingHorizontal: 6,
+  },
+});
