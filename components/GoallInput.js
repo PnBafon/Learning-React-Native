f6b79ee19b9834b85d0d9f625480ed201ff7553a
PnBafon <bafonprecious@gmail.com>
2025-03-15 01:44:41 +0100

    updated the styles of the input

--- components/GoallInput.js
+++ components/GoallInput.js
@@ -1,5 +1,45 @@
 import React from "react";
-
-const GoalItem = (props) => {
-  return <></>;
+import { TextInput, View, Button, StyleSheet } from "react-native";
+const GoallInput = ({ handleInputGoal, handleUpdateGoals, inputGoal }) => {
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      <TextInput
+        placeholder="Course goals"
+        style={{
+          borderBottomColor: "black",
+          borderBottomWidth: 1,
+          marginBottom: 3,
+          padding: 8,
+          width: "80%",
+        }}
+        value={inputGoal}
+        onChangeText={handleInputGoal}
+      />
+      <Button title="ADD" onPress={handleUpdateGoals} />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+
+  input: {
+    borderBottomWidth: 1,
+    borderBlockColor: "gray",
+    marginBottom: 3,
+    padding: 8,
+    width: "80%",
+  },
+});
+
+export default GoallInput;
--- day1/CourseObjectives.js
+++ day1/CourseObjectives.js
@@ -9,6 +9,7 @@ import {
 } from "react-native";
 
 import GoalItem from "../components/GoalItem";
+import GoallInput from "../components/GoallInput";
 
 export default function CourseObjectives() {
   const [inputGoal, setInputGoal] = useState("");
@@ -33,27 +34,12 @@ export default function CourseObjectives() {
   };
   return (
     <View style={{ paddingTop: 45, paddingHorizontal: 18 }}>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
-        <TextInput
-          placeholder="Course goals"
-          style={{
-            borderBottomColor: "black",
-            borderBottomWidth: 1,
-            marginBottom: 3,
-            padding: 8,
-            width: "80%",
-          }}
-          value={inputGoal}
-          onChangeText={handleInputGoal}
-        />
-        <Button title="ADD" onPress={handleUpdateGoals} />
-      </View>
+      <GoallInput
+        handleInputGoal={handleInputGoal}
+        handleUpdateGoals={handleUpdateGoals}
+        inputGoal={inputGoal}
+      />
+
       <FlatList
         keyExtractor={(item, index) => item.id}
         data={goals}
